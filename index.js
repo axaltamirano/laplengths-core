f5c08a591eb23fdd1fdc318a9e0701cdf59b24b2
Andrew Altamirano <hi@andrewaltamirano.com>
2020-01-25 19:22:58 -0500

    calc Ld and Ldh with applicable factors

---
+++ index.js
@@ -0,0 +1,333 @@
+"use strict";
+
+const codeEditions = ['318-14']
+
+const __globalDefaults = {
+	lightweightConcrete: false,
+	epoxyCoatedRebar: false,
+	epoxyCoverSatisfied: false,
+	hookedCoverSatisfied: false,
+	hookedConfinementSatisfied: false,
+	codeEdition: '318-14',
+	isMetric: false,
+	preset: 'imperial',
+	roundBy: 1
+}
+
+const __presetDefaults = {
+	"imperial": {
+		rebarList: [
+			['#3', 0.375],
+			['#4', 0.500],
+			['#5', 0.625],
+			['#6', 0.750],
+			['#7', 0.875],
+			['#8', 1.000],
+			['#9', 1.128],
+			['#10', 1.270],
+			['#11', 1.410],
+			['#14', 1.693],
+			['#18', 2.257]
+		],
+		fy: 60000,
+		fc: 4000,
+	},
+	"softMetric": {
+		rebarList: [
+			['No.10', 9.52],
+			['No.13', 12.7],
+			['No.16', 15.8],
+			['No.19', 19.05],
+			['No.22', 22.225],
+			['No.25', 25.4],
+			['No.29', 28.65],
+			['No.32', 32.25],
+			['No.36', 35.81],
+			['No.43', 43.0],
+			['No.57', 57.33]
+		],
+		fy: 420,
+		fc: 30,
+		isMetric: true
+	},
+	"hardMetric": {
+		rebarList: [
+			['8', 8],
+			['10', 10],
+			['12', 12],
+			['14', 14],
+			['16', 16],
+			['20', 20],
+			['25', 25],
+			['28', 28],
+			['32', 32],
+			['40', 40],
+			['50', 50]
+		],
+		fy: 420,
+		fc: 30,
+		isMetric: true
+	}
+}
+
+/**
+ * 
+ */
+class RebarLapLengthTable {
+	constructor(args = {}) {
+		let globalSettings = __globalDefaults
+		let preset = globalSettings.preset
+		let presetSettings = __presetDefaults[preset]
+		if ('preset' in args) {
+			preset = args.preset
+			if (preset in __presetDefaults) { 
+				presetSettings = __presetDefaults[preset]
+			} else {
+				throw new Error('Preset undefined')
+			}
+		}
+		if ('codeEdition' in args) {
+			if (!(args.codeEdition in codeEditions)) {
+				throw new Error('Code edition not recognized or supported')
+			}
+		}
+		
+		Object.assign(this, globalSettings, presetSettings, args)
+	}
+
+	/**
+	 * Returns sqrt of f`c with limitations per:
+	 * 	ACI 318-14 25.4.1.4
+	 * @returns {number} sqrt(fc) with max limitation set per 25.4.1.4
+	 */
+	calcSqrtFc() {
+
+		if (this.isMetric == true) {
+			return Math.min(8.3, Math.sqrt(this.fc))
+		} else {
+			return Math.min(100, Math.sqrt(this.fc))
+		}
+	}
+
+	/**
+	 * Copmutes lambda factor in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.3
+	 * @returns {number} ligthweight concrete modification factor, lambda
+	 */
+	calcLambdaFactor() {
+		return (this.lightweightConcrete)? 0.75 : 1.0
+	}
+
+	/**
+	 * Computes epoxy factor in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.3
+	 * @returns {number} epoxy modification factor, psi_e
+	 */
+	calcEpoxyFactor() {
+		if (this.epoxyCoatedRebar == true) {
+			return (this.epoxyCoverSatisfied)? 1.2 : 1.5
+		} else return 1.0
+	}
+
+	/**
+	 * Computes size factor in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.3
+	 * @param {number} db rebar diameter
+	 * @returns {number} Size modification factor, psi_s
+	 */
+	calcSizeFactor(db) {
+		if (this.isMetric == true)
+		{
+			return (db >= 22)? 1.0 : 0.8
+		} else {
+			return (db >= 0.875)? 1.0 : 0.8
+		}
+	}
+
+	/**
+	 * Computes casting position factor in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.3
+	 * @param {boolean} isTop Does bar placement meet "top" definition?
+	 * @returns {number} Casting position modification factor, psi_t
+	 */
+	calcPositionFactor(isTop) {
+		return (isTop)? 1.3 : 1.0
+	}
+
+	/**
+	 * Computes combined epoxy and position factors based on
+	 * allowable 1.7 maximum combined value in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.3
+	 * @param {boolean} isTop
+	 * @returns {number} Combined epoxy and casting position modification factors, psi_e * psi_t
+	 */
+	calcCombinedPositionAndEpoxyFactors(isTop) {
+		let epoxyFactor = this.calcEpoxyFactor()
+		let positionFactor = this.calcPositionFactor(isTop)
+		return Math.min(1.7, epoxyFactor * positionFactor)
+	}
+
+	/**
+	 * Computes lambda factor for hooked bars in accordance with:
+	 * 	ACI 318-14 Table 25.4.3.2
+	 * @returns {number} lightweight concrete modification factor, lambda
+	 */
+	calcHookedLambdaFactor() {
+		return (this.lightweightConcrete)? 0.75 : 1.0
+	}
+
+	/**
+	 * Computes epoxy factor for hooked bars in accordance with:
+	 * 	ACI 318-14 Table 25.4.3.2
+	 * @returns {number} hooked epoxy modification factor, psi_e
+	 */
+	calcHookedEpoxyFactor() {
+		return (this.epoxyCoatedRebar)? 1.2 : 1.0
+	}
+
+	/**
+	 * Returns cover factor for hooked bars in accordance with:
+	 * 	ACI 318-14 Table 25.4.3.2
+	 * @param {number} db bar diameter
+	 * @returns {number} hooked cover modification factor, psi_c
+	 */
+	calcHookedCoverFactor(db) {
+		if (this.hookedCoverSatisfied)
+		{
+			if (this.isMetric) {
+				return (db <= 35.81)? 0.7 : 1.0
+			} else {
+				return (db <= 1.41)? 0.7 : 1.0
+			}
+		} else return 1.0
+	}
+
+	/**
+	 * Returns confinement factor for hooked bars in accordance with:
+	 * 	ACI 318-14 Table 25.4.3.2
+	 * @param {number} db bar diameter
+	 * @returns {number} confining reinforcement modification factor, psi_r 
+	 */
+	calcHookedConfinementFactor(db) {
+		if (this.hookedConfinementSatisfied) {
+			if (this.isMetric) {
+				return (db <= 35.81) ? 0.8 : 1.0
+			} else {
+				return (db <= 1.41) ? 0.8 : 1.0
+			}
+		} else return 1.0
+	}
+
+	/**
+	 * Calculated bar development length in accordance with:
+	 * 	ACI 318-14 Table 25.4.2.2
+	 * 	ACI 318M-14 Table 25.4.2.2
+	 * @param {number} db bar diameter
+	 * @param {boolean} isTop does bar placement meet "top" definition
+	 * @param {boolean} meetsCover does bar meet spacing and cover?
+	 * @returns {number} development length, Ld
+	 */
+	calcDevelopmentLength(db, isTop, meetsCover) {
+		let sizeTrigger = (this.isMetric)? 19.05 : 0.75
+		var factor
+		if (meetsCover)
+		{
+			if (db <= sizeTrigger)
+			{
+				factor = (this.isMetric)? 2.1 : 25
+			} else {
+				factor = (this.isMetric)? 1.7 : 20
+			}
+		} else {
+			if (db <= sizeTrigger) {
+				factor = (this.isMetric)? 1.4 : (50/3)
+			} else {
+				factor = (this.isMetric)? 1.1 : (40/3)
+			}
+		}
+
+		let psi_p_e = this.calcCombinedPositionAndEpoxyFactors(isTop)
+		let lambda = this.calcLambdaFactor()
+		let sqrtFc = this.calcSqrtFc()
+
+		return db * this.fy * psi_p_e / (factor * lambda * sqrtFc)
+	}
+
+	/**
+	 * Calculates hooked tension development lengths in accordance with:
+	 * 	ACI 318-14 25.4.3.1
+	 * @param {number} db bar size 
+	 */
+	calcHookedDevelopmentLength(db) {
+		let lambda = this.calcHookedLambdaFactor()
+		let psi_e = this.calcHookedEpoxyFactor()
+		let psi_c = this.calcHookedCoverFactor(db)
+		let psi_r = this.calcHookedConfinementFactor(db)
+		let sqrtFc = this.calcSqrtFc()
+		let factor = (this.isMetric)? (1/0.24) : 50
+		let minValue = (this.isMetric)? Math.max(150, 8*db) : Math.max(6, 8*db)
+		let ldh = this.fy * db * psi_e * psi_c * psi_r / (factor * lambda * sqrtFc)
+
+		return Math.max(minValue, ldh)
+
+	}
+
+	/**
+	 * Helper function to round up numbers by set value
+	 * @param {number} number
+	 * @param {number} by increment to round up to (ex. 5 rounds up to next multiple of 5)
+	 * @returns {number} rounded number 
+	 */
+	roundUpTo(number, by = this.roundBy) {
+		return Math.ceil((number) / by) * by
+	}
+
+	getTable()
+	{
+		rows = []
+		for (row in this.rebarList)
+		{
+			name = row[0]
+			db = row[1]
+
+			rows.push({
+				barSize: name,
+				db: db,
+				area: Math.PI * db^2 / 4,
+				tensionTop: {
+					Ld: {
+						meetsCover: this.roundUpTo(this.calcDevelopmentLength(db, true, true)),
+						doesNotMeetCover: this.roundUpTo(this.calcDevelopmentLength(db, true, false))
+					},
+					LTS: {
+
+					}
+				},
+				tensionOther: {
+					Ld: {
+						meetsCover: this.roundUpTo(this.calcDevelopmentLength(db, false, true)),
+						doesNotMeetCover: this.roundUpTo(this.calcDevelopmentLength(db, false, false))
+					},
+					LTS: {
+
+					}
+				},
+				compression: {
+					Ld: {
+
+					},
+					LTS: {
+
+					}
+				},
+				tensionHook: {
+					Ldh: this.calcHookedDevelopmentLength(db)
+				}
+			})
+		}
+
+		return rows
+	}
+}
+
+module.exports = RebarLapLengthTable
---
+++ index.test.js
@@ -0,0 +1,310 @@
+LapLengthTable = require('./')
+
+test('Verify default settings', () => {
+	table = new LapLengthTable({
+		fc: 6000,
+		preset: 'imperial'
+	})
+	expect(table.fy).toEqual(60000)
+	expect(table.rebarList[0][0]).toEqual('#3')
+	expect(table.codeEdition).toEqual('318-14')
+
+	table = new LapLengthTable({
+		fc: 4000
+	})
+	expect(table.preset).toEqual('imperial')
+	expect(table.fy).toEqual(60000)
+	expect(table.rebarList[0][0]).toEqual('#3')
+	expect(table.codeEdition).toEqual('318-14')
+})
+
+test('Check code edition validation', () => {
+	expect(() => {
+		table = new LapLengthTable({
+			codeEdition: 'BadCode'
+		})
+	}).toThrow()
+})
+
+test('Check preset loading', () => {
+	expect(() => {
+		table = new LapLengthTable({
+			preset: 'Australia'
+		})
+	}).toThrow()
+
+	table = new LapLengthTable({
+		preset: 'softMetric'
+	})
+	expect(table.rebarList[0][0]).toEqual('No.10')
+
+	table = new LapLengthTable({
+		preset: 'hardMetric'
+	})
+	expect(table.rebarList[0][0]).toEqual('8')
+})
+
+test('Check rounding helper function', () => {
+	table = new LapLengthTable()
+	// first check with default settings
+	expect(table.roundBy).toEqual(1)
+	expect(table.roundUpTo(20.4)).toEqual(21)
+	expect(table.roundUpTo(20.0)).toEqual(20)
+	// test other rounding mutliples
+	expect(table.roundUpTo(22, 5)).toEqual(25)
+	expect(table.roundUpTo(21.7, 10)).toEqual(30)
+})
+
+test('Check sqrt(fc) helper function', () => {
+	table = new LapLengthTable({
+		fc: 4000
+	});
+	expect(table.calcSqrtFc()).toBeCloseTo(63.2, 1)
+	
+	table = new LapLengthTable({
+		fc: 6000
+	});
+	expect(table.calcSqrtFc()).toBeCloseTo(77.5, 1)
+
+	table = new LapLengthTable({
+		fc: 12000
+	});
+	expect(table.calcSqrtFc()).toEqual(100) // based on code limit
+
+	// metric checks
+	table = new LapLengthTable({
+		fc: 30,
+		isMetric: true
+	})
+	expect(table.calcSqrtFc()).toBeCloseTo(5.5, 1)
+
+	table = new LapLengthTable({
+		fc: 50,
+		isMetric: true
+	})
+	expect(table.calcSqrtFc()).toBeCloseTo(7.1, 1)
+
+	table = new LapLengthTable({
+		fc: 82,
+		isMetric: true
+	})
+	expect(table.calcSqrtFc()).toEqual(8.3)
+})
+
+test('Check lambda factor calculations', () => {
+	table = new LapLengthTable({
+		lightweightConcrete: false
+	})
+	expect(table.calcLambdaFactor()).toEqual(1.0)
+
+	table = new LapLengthTable({
+		lightweightConcrete: true
+	})
+	expect(table.calcLambdaFactor()).toEqual(0.75)
+})
+
+test('check Epoxy-coated factor calculations', () => {
+	table = new LapLengthTable({
+		epoxyCoatedRebar: false,
+		epoxyCoverSatisfied: false
+	})
+	expect(table.calcEpoxyFactor()).toEqual(1.0)
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: true,
+		epoxyCoverSatisfied: false
+	})
+	expect(table.calcEpoxyFactor()).toEqual(1.5)
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: true, 
+		epoxyCoverSatisfied: true})
+	expect(table.calcEpoxyFactor()).toEqual(1.2)
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: false,
+		epoxyCoverSatisfied: true})
+	expect(table.calcEpoxyFactor()).toEqual(1.0)
+})
+
+test('Check size modification factor calculations', () => {
+	table = new LapLengthTable({
+		isMetric: false
+	})
+	expect(table.calcSizeFactor(0.5)).toEqual(0.8)
+	expect(table.calcSizeFactor(0.75)).toEqual(0.8)
+	expect(table.calcSizeFactor(0.875)).toEqual(1.0)
+
+	table = new LapLengthTable({
+		isMetric: true
+	})
+	expect(table.calcSizeFactor(10)).toEqual(0.8)
+	expect(table.calcSizeFactor(16)).toEqual(0.8)
+	expect(table.calcSizeFactor(22)).toEqual(1.0)
+})
+
+test('Check position modification factor calculations', () => {
+	table = new LapLengthTable()
+	expect(table.calcPositionFactor(true)).toEqual(1.3)
+	expect(table.calcPositionFactor(false)).toEqual(1.0)
+})
+
+test('Check combined position and epoxy modification factors calcualation', () => {
+	table = new LapLengthTable({
+		epoxyCoatedRebar: false,
+		epoxyCoverSatisfied: false
+	})
+	expect(table.calcCombinedPositionAndEpoxyFactors(false)).toEqual(1.0) // 1.0 * 1.0
+	expect(table.calcCombinedPositionAndEpoxyFactors(true)).toEqual(1.3) // 1.3 * 1.0
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: true,
+		epoxyCoverSatisfied: false
+	})
+	expect(table.calcCombinedPositionAndEpoxyFactors(false)).toEqual(1.5) // 1.0 * 1.5
+	expect(table.calcCombinedPositionAndEpoxyFactors(true)).toEqual(1.7) // 1.3 * 1.5 > 1.7
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: true,
+		epoxyCoverSatisfied: true
+	})
+	expect(table.calcCombinedPositionAndEpoxyFactors(false)).toEqual(1.2) // 1.0 * 1.2
+	expect(table.calcCombinedPositionAndEpoxyFactors(true)).toEqual(1.56) // 1.3 * 1.2
+})
+
+test('Check hooked lambda factor calculations', () => {
+	table = new LapLengthTable({
+		lightweightConcrete: false
+	})
+	expect(table.calcHookedLambdaFactor()).toEqual(1.0)
+
+	table = new LapLengthTable({
+		lightweightConcrete: true
+	})
+	expect(table.calcHookedLambdaFactor()).toEqual(0.75)
+})
+
+test('check hooked epoxy-coated factor calculations', () => {
+	table = new LapLengthTable({
+		epoxyCoatedRebar: false
+	})
+	expect(table.calcHookedEpoxyFactor()).toEqual(1.0)
+
+	table = new LapLengthTable({
+		epoxyCoatedRebar: true
+	})
+	expect(table.calcHookedEpoxyFactor()).toEqual(1.2)
+})
+
+test('check tension development length calculations', () => {
+
+	// fc = 4000 psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 4000
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(19) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(25) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(48) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(62) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(81) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(105) // #14 top
+
+	// fc = 8000 psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 8000
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(14) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(18) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(34) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(44) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(57) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(74) // #14 top
+
+	// fc = 8000 psi, fy = 80,000psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 8000,
+		fy: 80000
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(18) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(24) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(45) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(59) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(76) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(99) // #14 top
+
+	// fc = 4000 psi liqhtweight, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 4000,
+		lightweightConcrete: true
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(26) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(33) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(64) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(83) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(108) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(140) // #14 top
+
+	// fc = 4000 psi epoxy coated rebar without additional clear cover
+	table = new LapLengthTable({
+		fc: 4000,
+		epoxyCoatedRebar: true,
+		epoxyCoverSatisfied: false
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(29) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(33) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(72) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(81) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(121) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(137) // #14 top
+
+	// fc = 4000 psi epoxy coated rebar additional clear cover satisfied
+	table = new LapLengthTable({
+		fc: 4000,
+		epoxyCoatedRebar: true,
+		epoxyCoverSatisfied: true
+	})
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, false, true))).toEqual(23) // #4 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(0.5, true, true))).toEqual(30) // #4 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, false, true))).toEqual(57) // #8 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.0, true, true))).toEqual(74) // #8 top
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, false, true))).toEqual(97) // #14 bottom
+	expect(table.roundUpTo(table.calcDevelopmentLength(1.693, true, true))).toEqual(126) // #14 top
+})
+
+test('check hooked bar tension development length calculations', () => {
+	
+	// fc = 4000psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 4000
+	})
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(0.5))).toEqual(10) // #4
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.0))).toEqual(19) // #8
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.693))).toEqual(33) // #14
+
+	// fc = 8000psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 8000
+	})
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(0.5))).toEqual(7) // #4
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.0))).toEqual(14) // #8
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.693))).toEqual(23) // #14
+
+	// fc = 4000psi, fy = 80,000psi, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 8000,
+		fy: 80000
+	})
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(0.5))).toEqual(9) // #4
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.0))).toEqual(18) // #8
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.693))).toEqual(31) // #14
+
+	// fc = 4000psi lightweight, non-epoxy coated rebar
+	table = new LapLengthTable({
+		fc: 4000,
+		lightweightConcrete: true
+	})
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(0.5))).toEqual(13) // #4
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.0))).toEqual(26) // #8
+	expect(table.roundUpTo(table.calcHookedDevelopmentLength(1.693))).toEqual(43) // #14
+
+})
